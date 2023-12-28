121d94855a7ec837f486fe02951127f9d882d67e
Silvia <152875675+SilviaEvafarina@users.noreply.github.com>
2023-12-29 00:21:49 +0800

    Update Equipment.java

--- VIVA3/src/viva3/Equipment.java
+++ VIVA3/src/viva3/Equipment.java
@@ -13,20 +13,41 @@ public class Equipment {
     //instances
     private Flashlight flashlight;
     private CloseDoor closeDoor;
-    
-    public Equipment[] createEquipment(Flashlight f, CloseDoor c){
-        this.flashlight = f;
-        this.closeDoor = c;
-        Equipment[] equipmentArray = null;
-        
-        return equipmentArray;
-        
-    }   
-    
+
+    public Equipment(Flashlight flashlight, CloseDoor closeDoor){
+        this.flashlight=flashlight;
+        this.closeDoor=closeDoor;
+    }    
+
     public void equipmentList(){
-        System.out.println("Equipment List: ");
-        System.out.println("Flashlight Battery: " +flashlight);
-        
-    }
+        System.out.println("Equipment List:");
+        System.out.println("Flashlight Battery: " +flashlight.getBattery());
+        System.out.println("CloseDoor: " +closeDoor.getDoorUses());
+    }    
+
+    public boolean useEquipment(String equipmentName, Monster monster){
+        if(equipmentName.equals("Flashlight")){
+            int batteryReduction = monster.getBatteryReduction();
+            int currentBattery = flashlight.getBattery();
+            if(currentBattery >= batteryReduction){
+                flashlight.reduceBattery(batteryReduction);
+                return true;
+            }else{
+                System.out.println("Not enough battery for flashlight!");
+                return false;
+        else if(equipmentName.equals("CloseDoor"){
+            int doorUses = closeDoor.getDoorUses();
+            if(doorUses > 0){
+                closeDoor.reduceDoorUses();
+                return true;
+            }else{
+                "System.out.println("No more door uses left!");
+                return false;
+            }
+        }else{
+            System.out.println("Invalid equipment name!");
+            return false;
+        }
+    }    
    
 }
