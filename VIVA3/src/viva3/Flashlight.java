026cbbe63d8834762e7455161b8a9a63116b9d29
Silvia <152875675+SilviaEvafarina@users.noreply.github.com>
2023-12-28 23:06:42 +0800

    Add files via upload

---
+++ VIVA3/src/viva3/Flashlight.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package VIVA3;
+
+/**
+ *
+ * @author SILVIA EVAFARINA
+ */
+public class Flashlight {
+    
+    private String name;
+    
+    public Flashlight(){
+        this.name= "Flashlight";
+    }
+    
+    public String getName(){
+        return name;
+        
+    }
+    
+    
+}
