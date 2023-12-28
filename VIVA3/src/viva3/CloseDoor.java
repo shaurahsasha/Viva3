f880793a428ef7ca06c94c01699880cc280d3be8
Silvia <152875675+SilviaEvafarina@users.noreply.github.com>
2023-12-28 23:09:00 +0800

    Add files via upload

---
+++ VIVA3/src/viva3/CloseDoor.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package VIVA3;
+
+/**
+ *
+ * @author SILVIA EVAFARINA
+ */
+public class CloseDoor {
+    
+    private String name;
+    
+    public String getName(){
+        this.name= "CloseDoor";
+        
+        return name;
+        
+        
+    }
+
+}
